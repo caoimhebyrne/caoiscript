8cee3751bcfd6078b41ada4b7bbfa0300559bc6d
Caoimhe <caoimhebyrne06@gmail.com>
2023-06-25 23:52:29 +0100

    Tests: Use line `1` as the initial line

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -50,7 +50,7 @@ impl TestRunner {
                             let column = error.location.column;
 
                             println!("====================");
-                            println!("Error at line {} column {}:", line, column - 1);
+                            println!("Error at line {} column {}:", line + 1, column - 1);
                             println!("{}", self.script.lines().nth(line).unwrap());
                             println!("{}^", " ".repeat(column));
                             println!("{}{}", " ".repeat(column), error.message);
